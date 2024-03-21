7e0f8c279c3a2b3fdd379928bd69503d65a51be3
Lima Junior <metalivredev@outlook.com>
2024-03-21 18:05:30 -0300

    variaveis

---
+++ scripts/variavel.js
@@ -0,0 +1,8 @@
+const EU_SOU_UMA_CONSTANTE = 4.1234;
+
+let predefinicao = 'Estou pre setada em alguma valor';
+var posDefniicao = 10;
+
+console.log(EU_SOU_UMA_CONSTANTE+'\n');
+console.log(predefinicao+'\n');
+console.log(posDefniicao);
